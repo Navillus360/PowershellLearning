b2eb11cf5f391c49a42e49438de5f1fc9ba234fc
Navillus360 <83305754+Navillus360@users.noreply.github.com>
2025-04-21 19:44:02 +0100

    Add menu method
    Added a menu method which allows the user to create a file or a folder. However, at this point, either item will be created in the same directory as the script.

--- FileCreation.ps1
+++ FileCreation.ps1
@@ -1,4 +1,16 @@
-$currentDirectory = Get-Location
-$fileToCreate = $currentDirectory,"\HelloWorld.txt" -join""
-Write-Host $test
-New-Item -Path "$fileToCreate" -ItemType File
+$Global:currentDirectory = Get-Location
+$Global:fileToCreate = $currentDirectory, "\HelloWorld.txt" -join""
+$Global:folderToCreate = $currentDirectory, "\TestFolder" -join""
+
+function Menu {
+ $running = true
+ while ($running) {
+   $userInput = Read-Host "1: Create File `n2: Create Folder `n3: Exit"
+   switch ($userInput) {
+    1 {  New-Item -Path $Global:fileToCreate -ItemType File }
+    2 {  New-Item -Path $Global:fileToCreate -ItemType Folder}
+    3 {$userInput = false}
+    Default {Write-Host "Error: Invalid input"}
+   }
+ }
+}
