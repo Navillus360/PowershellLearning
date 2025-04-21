6c5aee8096d0ef0d000a7fc08d44c4b5f8718f7f
Navillus360 <83305754+Navillus360@users.noreply.github.com>
2025-04-21 20:31:29 +0100

    Add item setup
    Added a more dynamic way to allow the user to name their file/folder and decide it's location.
    
    Also separated the creation of the file/folder into their respective methods which currently print out the item name and location from the user input.

--- FileCreation.ps1
+++ FileCreation.ps1
@@ -3,14 +3,40 @@ $Global:fileToCreate = $currentDirectory, "\HelloWorld.txt" -join""
 $Global:folderToCreate = $currentDirectory, "\TestFolder" -join""
 
 function Menu {
- $running = true
+ $running = $true
  while ($running) {
-   $userInput = Read-Host "1: Create File `n2: Create Folder `n3: Exit"
+   $userInput = Read-Host "1: Create File `n2: Create Folder `n3: Exit `n>"
    switch ($userInput) {
-    1 {  New-Item -Path $Global:fileToCreate -ItemType File }
-    2 {  New-Item -Path $Global:fileToCreate -ItemType Folder}
-    3 {$userInput = false}
+    1 { ItemSetup 1 }
+    2 { ItemSetup 2 }
+    3 {$running = $false}
     Default {Write-Host "Error: Invalid input"}
    }
  }
 }
+
+function ItemSetup($answer) {
+ $itemName = Read-Host "What would you like to name this File/Folder? `n>"
+ $itemLocation = Read-Host "Where would you like to create the File/Folder? `n>"
+ switch ($answer) {
+  1 {CreateFile $itemName, $itemLocation}
+  2 {CreateFolder $itemName, $itemLocation}
+ }
+}
+
+function CreateFile($fileName ,$fileLocation){
+ Write-Host "$folderName $folderLocation"
+ # Creates the file
+ # New-Item -Path $Global:fileToCreate -ItemType File
+}
+
+function CreateFolder($folderName ,$folderLocation){
+ Write-Host "$folderName $folderLocation"
+ # Creates the folder
+ # New-Item -Path $Global:fileToCreate -ItemType Folder
+}
+
+# <!> How to get the users username <!>
+# Write-Host (Get-ChildItem Env:\USERNAME).value 
+
+Menu
