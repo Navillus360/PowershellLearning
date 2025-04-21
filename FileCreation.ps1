03170c8e4f1be142c9d34a506f33b55f90e5d9d8
Navillus360 <83305754+Navillus360@users.noreply.github.com>
2025-04-21 21:55:26 +0100

    File and folder creation
    Added multiple ways for a file/folder to be created depending on the users input with default items being backups/temporary solutions. This meant changing the print commands to display whether the user gave a location and/or name for the item.

--- FileCreation.ps1
+++ FileCreation.ps1
@@ -1,42 +1,87 @@
-$Global:currentDirectory = Get-Location
-$Global:fileToCreate = $currentDirectory, "\HelloWorld.txt" -join""
-$Global:folderToCreate = $currentDirectory, "\TestFolder" -join""
+$Global:defaultFile = "\TextFile.txt"
+$Global:defaultFolder = "\TestFolder"
+$Global:defaultDirectory = Get-Location
+$Global:defaultItem = $Global:defaultDirectory, $Global:defaultFile -join ""
 
 function Menu {
- $running = $true
- while ($running) {
-   $userInput = Read-Host "1: Create File `n2: Create Folder `n3: Exit `n>"
-   switch ($userInput) {
-    1 { ItemSetup 1 }
-    2 { ItemSetup 2 }
-    3 {$running = $false}
-    Default {Write-Host "Error: Invalid input"}
-   }
- }
+  Write-Host "$Global:defaultDirectory"
+  $running = $true
+  while ($running) {
+    $userInput = Read-Host "1: Create File `n2: Create Folder `n3: Exit `n>"
+    switch ($userInput) {
+      1 { ItemSetup 1 }
+      2 { ItemSetup 2 }
+      3 { $running = $false }
+      Default { Write-Host "Error: Invalid input" }
+    }
+  }
 }
 
-function ItemSetup($answer) {
- $itemName = Read-Host "What would you like to name this File/Folder? `n>"
- $itemLocation = Read-Host "Where would you like to create the File/Folder? `n>"
- switch ($answer) {
-  1 {CreateFile $itemName, $itemLocation}
-  2 {CreateFolder $itemName, $itemLocation}
- }
+function ItemSetup() {
+  param (
+    [string] $answer
+  )
+  $itemName = Read-Host "What would you like to name this File/Folder? `n>"
+  $itemLocation = Read-Host "Where would you like to create the File/Folder? `n<!> If you input nothing for the File/Folder name, a default name and location will be chosen for you<!> `n>"
+  switch ($answer) {
+    1 { CreateFile $itemName, $itemLocation }
+    2 { CreateFolder $itemName, $itemLocation }
+  }
 }
 
-function CreateFile($fileName ,$fileLocation){
- Write-Host "$folderName $folderLocation"
- # Creates the file
- # New-Item -Path $Global:fileToCreate -ItemType File
+function CreateFile() {
+  param (
+    [string] $fileName,
+    [string] $fileLocation
+  )
+
+  if ($fileName -eq " " -and $fileLocation -eq "") {
+    Read-Host "No location and name given"
+    New-Item -Path $Global:defaultItem -ItemType File
+  }
+  elseif ($fileName -eq "") {
+    Read-Host "No name but location given"
+    $fileToCreate = $fileLocation, $Global:defaultFile -join ""
+    New-Item -Path $fileToCreate -ItemType File
+  }
+  elseif ($fileLocation -eq "") {
+    Read-Host "No location but name given"
+    $fileToCreate = $Global:defaultFolder, $fileName -join ""
+    New-Item -Path $fileToCreate -ItemType File
+  }
+  else {
+    $fileToCreate = $fileName, $fileLocation -join ""
+    New-Item -Path $fileToCreate -ItemType File
+  }
+  Write-Host "<!>File Created<!>"s
 }
 
-function CreateFolder($folderName ,$folderLocation){
- Write-Host "$folderName $folderLocation"
- # Creates the folder
- # New-Item -Path $Global:fileToCreate -ItemType Folder
+function CreateFolder() {
+  param (
+    [string] $folderName,
+    [string] $folderLocation
+  )
+  
+  if ($folderName -eq " " -and $folderLocation -eq "" ) {
+    Read-Host "No location and name given"
+    $folderToCreate = $Global:defaultDirectory, $Global:defaultFolder -join ""
+    New-Item -Path $folderToCreate -ItemType Directory
+  }
+  elseif ($folderName -eq "") {
+    Read-Host "No name but location given"
+    $folderToCreate = $folderLocation, $Global:defaultFolder -join ""
+    New-Item -Path $folderToCreate -ItemType Directory
+  }
+  elseif ($folderLocation -eq "") {
+    Read-Host "No location but name given"
+    $folderToCreate = $folderName, $Global:defaultDirectory -join ""
+    New-Item -Path $folderToCreate -ItemType Directory
+  }
+  else {
+    $folderToCreate = $folderName, $folderLocation
+    New-Item -Path $folderToCreate -ItemType Directory
+  }
+  Write-Host "<!>Folder Created<!>"
 }
-
-# <!> How to get the users username <!>
-# Write-Host (Get-ChildItem Env:\USERNAME).value 
 
 Menu
