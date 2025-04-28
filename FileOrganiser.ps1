1e8e43de046e914c16d28c2eac3cdf226fe9bb98
Navillus360 <83305754+Navillus360@users.noreply.github.com>
2025-04-28 14:20:02 +0100

    Desktop and download organisation
    Set up the cleanup method using a userChoice parameter which executes the code to clean-up the users desktop or the download folder depending on their choice in the menu.  Additionally, added in a check to create downloaded documents/images if they do not exist as this can help the user distinguish which files are from the desktop or the downloads folder.
    
    Furthermore, added a location select method which allows the user to choose a location of their own to be cleaned up.

--- FileOrganiser.ps1
+++ FileOrganiser.ps1
@@ -1,6 +1,20 @@
+#region Variables
 $desktop = Get-ChildItem -Path $env:USERPROFILE\Desktop -File -Recurse
+
+$downloads = Get-ChildItem -Path $env:USERPROFILE\Downloads -File -Recurse
+
 $imgTypes = ".png", ".jpg", ".jpeg", ".gif", ".bmp", ".tiff", ".webp"
+
 $docTypes = ".pdf", ".doc", ".docx", ".xls", ".xlsx", ".txt"
+
+$documents = "$env:USERPROFILE\Downloads"
+
+$pictures = "$env:USERPROFILE\Pictures"
+
+$downloadedImages = "$env:USERPROFILE\Pictures\DownloadedImages"
+
+$downloadedDocuments = "$env:USERPROFILE\Documents\DownloadedImages"
+#endregion
 
 function Menu {
  $running = $true
@@ -9,7 +23,7 @@ function Menu {
   switch ($choice) {
    1 { Cleanup 1 }
    2 { Cleanup 2 }
-   3 { Cleanup 3 }
+   3 { LocationSelect }
    4 { exit }
    Default { Write-Host "Error: Invalid input" }
   }
@@ -20,7 +34,44 @@ function Cleanup {
  param (
   [byte] $userChoice
  )
- 
+
+ switch ($userChoice) {
+  #Cleans up the desktop
+  1 {
+   foreach ($file in $desktop) {
+    if ($file.Extension -in $imgTypes) {
+     Move-Item -Path $desktop, $file -join "" -Destination $pictures, $file -join ""
+    }
+    elseif ($file.Extension -in $docTypes) {
+     Move-Item -Path $desktop, $file -join "" -Destination $documents, $file -join ""
+    }
+   } 
+  }
+
+  #Cleans up the downloads
+  2 {
+   #Creates the downloaded images/docs if the dir does not exist
+   if (!(Test-Path -Path $downloadedImages)) {
+    New-Item -ItemType Directory $downloadedImages | Out-Null
+   }
+   if (!(Test-Path -Path $downloadedDocuments)) {
+    New-Item -ItemType Directory $downloadedDocuments | Out-Null
+   }
+
+   foreach ($file in $downloads) {
+    if ($file.Extension -in $imgTypes) {
+     Move-Item -Path $downloads, $file -join"" -Destination $downloadedImages, $file -join""
+    }
+    elseif ($file.Extension -in $downloads) {
+     Move-Item -Path $downloads, $file -join"" -Destination $downloadedDocuments, $file -join""
+    }
+   } 
+  }
+ }
+}
+
+function LocationSelect {
+ $userLocaion = Read-Host "If you wish to return to the menu, enter 1. Otherwise, enter the full location you wish to cleanup `n>"
 }
 
 Menu
