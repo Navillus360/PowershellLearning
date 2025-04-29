98427e958482f1cc0202665bbc258b0b8ab76b91
Navillus360 <83305754+Navillus360@users.noreply.github.com>
2025-04-29 10:34:59 +0100

    Delete user and admin right check
    Implemented a way for the user to remove others users and their profile to mimic wiping their account from the system but also added a safeguard to avoid the user from deleting themself.
    
    Furthermore, added a simple admin check within the menu to ensure that the user running the script has admin privileges.

--- UserCreation.ps1
+++ UserCreation.ps1
@@ -1,13 +1,11 @@
-#Checks if the user is an admin
-$user = [Security.Principle.WindowsIdentity]::GetCurrent()
-$principlal = New-Object Security.Principle.WindowsPrincipal $user
-$isAdmin = $principlal.IsInRole([Security.Principle.WindowsBuiltInRole]::Administrator)
 
 function Menu {
  $running = $true
+ $currentUser = whoami.exe
+ $Admins = Get-LocalGroupMember -Name Administrators | 
+ Select-Object -ExpandProperty name
  while ($running) {
-  if ($isAdmin) {
-   echo $isAdmin
+  if ($admins -Contains $currentUser) {
    $userChoice = Read-Host "1: Add user `n2: Delete user `n3: Exit `n>"
    switch ($userChoice) {
     1 { CreateUser; exit }
@@ -15,10 +13,11 @@ function Menu {
     3 { exit }
     Default { Write-Host "Error: Invalid input" }
    }
-   else {
-    Write-Host "<!>Error: You do not have the admin rights to use this script! Please login into an account which has admin rights or change your privlages<!>"
-    exit
-   }
+  }
+  else {
+   Write-Host "<!>Error: You do not have the admin rights to use this script! Please login into an account which has admin rights or change your privlages<!>"
+   $running = $false
+   exit
   }
  }
 }
@@ -29,19 +28,28 @@ function CreateUser {
  $displayName = Read-Host "Enter displayName >"
  $adminRights = Read-Host "Set user to admin? [Y/n]"
  if ($adminRights -eq "Y") {
-  echo "Admin rights"
+  New-LocalUser -Name $userName -Password $password -FullName $displayName
  }
  elseif ($adminRights -eq "n") {
-  echo "No admin rights"
+  New-LocalUser -Name $userName -Password $password -FullName $displayName
+  Add-LocalGroupMember -Group Adminstrators -Member $userName
  }
- # New-LocalUser -Name $userName -Password $password -FullName $displayName
- # Add-LocalGroupMember -Group Adminstrators -Member $userName
+ Write-Host "User created"
 }
 
 function DeleteUser {
- # Remove-LocalUser -Name $userName
- # Get-CimInstance -Class Win32_UserProfile | Where-Object { $_.LocalPath -like $userName } | Remove-CimInstance
- # Remove-Item -Path "C:\Users\$username" -Recurse -Force
+ $userName = Read-Host "Enter username >"
+ if ((Get-LocalUser).Name -contains $userName) {
+  Remove-LocalUser -Name $userName
+  Get-CimInstance -Class Win32_UserProfile | Where-Object { $_.LocalPath -like $userName } | Remove-CimInstance
+  Remove-Item -Path "C:\Users\$username" -Recurse -Force
+  Write-Host "User removed"
+ }
+ elseif ($userName -eq $env:USERPROFILE) {
+  Write-Host "You cannot delete yourself!"
+ }
+ else {
+  Write-Host "Error: Name invalid or user does not exist"
+ }
 }
 
-Menu
