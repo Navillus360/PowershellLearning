4544f5598e46b23f9300f6feb7d19e2eca98a715
Navillus360 <83305754+Navillus360@users.noreply.github.com>
2025-05-05 19:55:26 +0100

    Clean-up separation and FolderBrowserDialog
    Added in separated clean-up functions to avoid any overlap of cleaning other destinations at the same time (e.g. the program organises the desktop and downloads although the user chose a location on their E: drive.)
    
    Also implemented a FolderBrowserDialog object to allow the process of the user choosing their folder to be easier rather than inputting the full path.
    
    Lastly, organised variables such as the downloaded images/documents locations to be created within the functions and applied the same method of creating the folders within the users selected location to easily find them.

--- FileOrganiser.ps1
+++ FileOrganiser.ps1
@@ -1,19 +1,7 @@
 #region Variables
-$desktop = Get-ChildItem -Path $env:USERPROFILE\Desktop -File -Recurse
-
-$downloads = Get-ChildItem -Path $env:USERPROFILE\Downloads -File -Recurse
-
 $imgTypes = ".png", ".jpg", ".jpeg", ".gif", ".bmp", ".tiff", ".webp"
 
 $docTypes = ".pdf", ".doc", ".docx", ".xls", ".xlsx", ".txt"
-
-$documents = "$env:USERPROFILE\Downloads"
-
-$pictures = "$env:USERPROFILE\Pictures"
-
-$downloadedImages = "$env:USERPROFILE\Pictures\DownloadedImages"
-
-$downloadedDocuments = "$env:USERPROFILE\Documents\DownloadedImages"
 #endregion
 
 function Menu {
@@ -21,8 +9,8 @@ function Menu {
  while ($running) {
   $choice = Read-Host "1: Desktop `n2: Downloads `n3: Custom location`n4: Exit `n>"
   switch ($choice) {
-   1 { Cleanup 1; exit }
-   2 { Cleanup 2; exit }
+   1 { AutoCleanup 1; exit }
+   2 { AutoCleanup 2; exit }
    3 { LocationSelect }
    4 { exit }
    Default { Write-Host "Error: Invalid input" }
@@ -30,19 +18,29 @@ function Menu {
  }
 }
 
-function Cleanup {
+function AutoCleanup {
  param (
   [byte] $userChoice
  )
+ 
+ $desktop = Get-ChildItem -Path $env:USERPROFILE\Desktop -File -Recurse
+ $downloads = Get-ChildItem -Path $env:USERPROFILE\Downloads -File -Recurse
+ $documents = "$env:USERPROFILE\Downloads"
+ $pictures = "$env:USERPROFILE\Pictures"
+
+ $downloadedImages = "$env:USERPROFILE\Pictures\DownloadedImages"
+ $downloadedDocuments = "$env:USERPROFILE\Documents\DownloadedImages"
 
  switch ($userChoice) {
   #Cleans up the desktop
   1 {
    foreach ($file in $desktop) {
+    #If files are images
     if ($file.Extension -in $imgTypes) {
      Move-Item -Path $desktop, $file -join "" -Destination $pictures, $file -join ""
     }
     elseif ($file.Extension -in $docTypes) {
+     #If files are documents
      Move-Item -Path $desktop, $file -join "" -Destination $documents, $file -join ""
     }
    }
@@ -61,9 +59,11 @@ function Cleanup {
 
    foreach ($file in $downloads) {
     if ($file.Extension -in $imgTypes) {
+     #If files are images
      Move-Item -Path $downloads, $file -join"" -Destination $downloadedImages, $file -join""
     }
-    elseif ($file.Extension -in $downloads) {
+    elseif ($file.Extension -in $docTypes) {
+     #If files are documents
      Move-Item -Path $downloads, $file -join"" -Destination $downloadedDocuments, $file -join""
     }
    } 
@@ -74,24 +74,43 @@ function Cleanup {
 }
 
 function LocationSelect {
- $userLocaion = Read-Host "If you wish to return to the menu, enter 1. Otherwise, enter the full location you wish to cleanup `n>"
- if ($userLocaion -eq 1) {
-  Menu
+ Add-Type -AssemblyName System.Windows.Forms
+ $FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
+ $null = $FolderBrowser.ShowDialog()
+ $ChosenPath = $FolderBrowser.SelectedPath
+ if ($null -ne $FolderBrowser.SelectedPath) {
+  CustomCleanup $ChosenPath
  }
- if (!(Test-Path -Path $userLocaion)) {
-  Write-Host "Error: The location does not exist! Please choose another."
- }
- else {
-  foreach ($file in $userLocaion) {
-   if ($file.Extension -in $imgTypes) {
-    Move-Item -Path $userLocaion, $file -join"" -Destination $userLocaion, $file -join""
-   }
-   elseif ($file.Extension -in $downloads) {
-    Move-Item -Path $downloads, $file -join"" -Destination $downloadedDocuments, $file -join""
-   }
-  } 
- }
+}
 
+function CustomCleanup {
+ param (
+  [string] $userPath
+ )
+
+ #Creates custom folders in the selected path to move the doc/images into
+ $customDocuments = "$userPath\Documents"
+ $customImages = "$userPath\Images"
+
+ #Creates the custom folders if they do not exist
+ if (!(Test-Path -Path $customDocuments)) {
+  New-Item -ItemType Directory $customDocuments | Out-Null
+ }
+ if (!(Test-Path -Path $customImages)) {
+  New-Item -ItemType Directory $customImages | Out-Null
+ }
+ 
+ foreach ($file in $userPath) {
+  #If files are images
+  if ($file.Extension -in $imgTypes) {
+   Move-Item -Path $userPath, $file -join"" -Destination $customImages, $file -join""
+  }
+
+  #If files are documents
+  elseif ($file.Extension -in $docTypes) {
+   Move-Item -Path $userPath, $file -join"" -Destination $customDocuments, $file -join""
+  }
+ } 
  Write-Host "$userLocation organised"
 }
 
